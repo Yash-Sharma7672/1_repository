cc299c2e21865ad3b74c3a730879f85a6540a00e
Yash Sharma <yashhsharma7672@gmail.com>
2025-08-03 00:31:42 -0400

    new changes

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 const age=30
-const name="Yash Sharma"
+const name1="Yash Sharma"
 const age = 8
+
+function sayName() {
+    console.log('My name is '+ name1)
+}   
+
+function newfn() {
+    console.log('hey I am a new function')
+}
